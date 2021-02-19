f4ebe5a1b5323a5f574aa318545cf5ca3a690f97
Olivier Pironi <pironiolivier@gmail.com>
2021-02-19 19:56:48 -0300

    Começando novo curso de JS

---
+++ Front-end/JS/introducao-javascript/js/principal.js
@@ -0,0 +1,48 @@
+
+var titulo = document.querySelector(".titulo");
+console.log(titulo.textContent);
+titulo.textContent = "Nutricionismo";
+
+
+var pacientes = document.querySelectorAll(".paciente");
+
+for(i=0;i<pacientes.length;i++){
+
+    var paciente=pacientes[i]
+
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+
+    var tdImc = paciente.querySelector(".info-imc");
+
+    pesoEhValido = true;
+    alturaEhValida = true;
+
+    if(peso<=0 || peso>=1000){
+        console.log("Peso inválido")
+        console.log(peso)
+        pesoEhValido = false;
+        tdImc.textContent="Peso inválido";
+        tdImc.textContent="Altura inválida";
+        paciente.classList.add("paciente-invalido");
+
+    };
+
+    if(altura<=0 || altura>=3){
+        console.log("Altura inválido")
+        console.log(altura)
+        alturaEhValida = false;
+        tdImc.textContent="Altura inválida";
+        paciente.classList.add("paciente-invalido");
+    };
+
+    if( alturaEhValida && pesoEhValido){
+        var imc= peso/(altura*altura);
+        tdImc.textContent=imc.toFixed(2);
+    } 
+
+
+}
