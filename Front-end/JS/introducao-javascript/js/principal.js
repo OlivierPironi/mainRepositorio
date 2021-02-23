409426f7bd50d3231594f0cb9fc854d5dd322fd5
Olivier Pironi <pironiolivier@gmail.com>
2021-02-23 17:04:19 -0300

    Avançando em JS

--- Front-end/JS/introducao-javascript/js/principal.js
+++ Front-end/JS/introducao-javascript/js/principal.js
@@ -26,7 +26,6 @@ for(i=0;i<pacientes.length;i++){
         console.log(peso)
         pesoEhValido = false;
         tdImc.textContent="Peso inválido";
-        tdImc.textContent="Altura inválida";
         paciente.classList.add("paciente-invalido");
 
     };
@@ -46,3 +45,40 @@ for(i=0;i<pacientes.length;i++){
 
 
 }
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click",function(event){
+        event.preventDefault();
+        var form = document.querySelector("#form-adiciona");
+
+        var nome = form.nome.value;
+        var peso = form.peso.value;
+        var altura = form.altura.value;
+        var gordura = form.gordura.value;
+        
+        var pacienteTr = document.createElement("tr");
+        console.log(pacienteTr)
+        
+        var nomeTd = document.createElement("td");
+        var pesoTd = document.createElement("td");
+        var alturaTd = document.createElement("td");
+        var gorduraTd = document.createElement("td");
+        var imcTd = document.createElement("td");
+        
+        nomeTd.textContent= nome;
+        pesoTd.textContent= peso;
+        alturaTd.textContent= altura;
+        gorduraTd.textContent= gordura;
+        
+        pacienteTr.appendChild(nomeTd);
+        pacienteTr.appendChild(pesoTd);
+        pacienteTr.appendChild(alturaTd);
+        pacienteTr.appendChild(pesoTd);
+        pacienteTr.appendChild(imcTd);
+
+        var tabela = document.querySelector("#tabela-pacientes")
+
+        tabela.appendChild=(pacienteTr);
+    }
+);
+
